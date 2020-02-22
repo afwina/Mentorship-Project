d805c5c10ecd1f9dc07fd5f4fdf2bbe010b44ded
afwina <athirahafwina@gmail.com>
2020-02-22 15:34:01 -0400

    Player moves to touch

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if there is a touch, move to it
+        if (Input.touchCount > 0)
+        {
+            Vector2 newPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            transform.position = newPoint;
+        }
+
+    }
+}
