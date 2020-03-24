84bbb2506d7d73aaf604dc26d3d1234aac8cc50b
afwina <athirahafwina@gmail.com>
2020-03-24 16:13:34 -0300

    Update with files from studio

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
+    public float speed;
+
+    Vector2 startPos;
+    Rigidbody2D rb;
+    Camera cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if there is a touch, move to it
-        if (Input.touchCount > 0)
+        if (Input.GetMouseButton(0))
         {
-            Vector2 newPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-            transform.position = newPoint;
+            Vector2 touchPos = cam.ScreenToWorldPoint(Input.mousePosition);
+            float newX = Vector2.MoveTowards(transform.position, touchPos, speed).x;
+            transform.position = new Vector2(newX, transform.position.y);
         }
+        //else if (Input.GetMouseButton(39))
+        //{
+        //    Vector2 touchPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        //    float newX = Vector2.MoveTowards(transform.position, touchPos, speed).x;
+        //    transform.position = new Vector2(newX, transform.position.y);
 
+        //}
     }
 }
---
+++ Assets/Scripts/projMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projMovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveUp();
+    }
+
+    private void moveUp()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+    }
+
+    private void OnBecameInvisible()
+    {
+        Debug.Log("offscreen");
+        Destroy(gameObject);
+    }
+}
